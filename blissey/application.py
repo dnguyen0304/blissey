336c1aaf0f455f76b357a4e0c58154dc9857577f
Duy Nguyen <dnguyen0304@gmail.com>
2017-03-15 14:57:21 -0400

    ME-544: Changed to dedicated entry point

--- blissey/application.py
+++ blissey/application.py
@@ -4,21 +4,11 @@ import collections
 import datetime
 import logging
 import re
-import sys
 
 import dateutil.parser
 import jira
-import twilio.twiml
 
 import blissey
-
-if sys.version_info == (2, 7):
-    import httplib as HttpStatusCode
-    import urlparse as urllib_parse
-    str = unicode
-elif sys.version_info >= (3, 0):
-    import http.client as HttpStatusCode
-    import urllib.parse as urllib_parse
 
 
 class BlisseyService(object):
@@ -172,34 +162,3 @@ class BlisseyService(object):
 
         return todays_note
 
-
-def handler(event, context):
-
-    blissey_service = BlisseyService()
-
-    query = urllib_parse.parse_qs(event['body'])
-    message = query['Body'][0]
-
-    error_message = 'NoteAdd is encountering an error <{}>.'
-
-    try:
-        blissey_service.add_note(message=message)
-        status_code = HttpStatusCode.CREATED
-        message_ = 'The note was added successfully.'
-    except LookupError as error:
-        blissey_service.logger.debug(msg=error_message.format(str(error)))
-        status_code = HttpStatusCode.UNPROCESSABLE_ENTITY
-        message_ = str(error)
-    except ValueError as error:
-        blissey_service.logger.debug(msg=error_message.format(str(error)))
-        status_code = HttpStatusCode.BAD_REQUEST
-        message_ = str(error)
-
-    reply = twilio.twiml.Response()
-    reply.message(message_)
-
-    response = {'statusCode': status_code,
-                'headers': {'Content-Type': 'text/xml'},
-                "body": str(reply)}
-
-    return response
